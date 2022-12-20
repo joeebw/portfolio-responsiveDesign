ac117d5afb001d6b4521b6ad7811d6c0af479bab
joeebw <joeegaarciia@gmail.com>
2022-12-19 19:49:38 -0600

    mobile

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const burguerNav = document.querySelector('#burguerNav');
+const navMobile = document.querySelector('#nav-mobile');
 
+const showBurguerNav = () => {
+    const containClass = navMobile.classList.contains('-translate-x-full');
+    
+    if(!containClass) return navMobile.classList.add('-translate-x-full');
+    navMobile.classList.remove('-translate-x-full');
+
+}
+
+burguerNav.addEventListener('click', showBurguerNav);
